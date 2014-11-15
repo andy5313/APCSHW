079c5c0a6b7b46e58024f282157ccd19740107f1
andy5313 <andyz5313@yahoo.com>
2014-11-15 15:41:19 -0500

    Updated Super Array

---
+++ 01SuperArray/SuperArray.java
@@ -0,0 +1,151 @@
++public class SuperArray{
++    private int numElements;
++    private Object[] SuperArray;
++       
++    public SuperArray(){
++	SuperArray = new Object[10];
++	numElements = 0;
++    }
++    
++    public SuperArray(int capacity){
++	SuperArray = new Object[capacity];
++	numElements = 0; 
++    }
++
++    public String toString(){
++	int a = 0;
++	String s = "[ ";
++	while (a<SuperArray.length){
++	    s += SuperArray[a];
++	    s += " ";
++	    a ++;
++	}
++	return s += "]";
++    }
++
++    public void add(Object e){  
++	if (SuperArray[SuperArray.length-1]== null){
++	    SuperArray[numElements] = e;
++	    numElements ++;
++	} else {
++	    Object[] copy = new Object[SuperArray.length+1];
++	    int i = 0;
++	    while (i<SuperArray.length){
++		copy[i] = SuperArray[i];
++		i ++;
++	    }
++	    copy[SuperArray.length]=e;
++	    SuperArray = copy;
++	    numElements ++;
++	}
++    }
++
++    public void add(int index, Object o){
++	if (SuperArray[index] == null){
++	    SuperArray[index] = o;
++	    numElements ++;
++	} else if (SuperArray[SuperArray.length-1] != null) {
++	    Object[] copy = new Object[SuperArray.length+1];
++	    int i = 0;
++	    while (i<index){
++		copy[i] = SuperArray[i];
++		i++;
++	    }
++	    copy[i] = o;
++	    while (i<SuperArray.length){
++		copy[i+1] = SuperArray[i];
++		i ++;
++	    }
++	    SuperArray = copy;
++	    numElements++;
++	} else {
++	    Object[] copy = new Object[SuperArray.length+1];
++	    int i = 0;
++	    while (i<index){
++		copy[i] = SuperArray[i];
++		i++;
++	    }
++	    copy[i] = o;
++	    while (i<SuperArray.length-1){
++		copy[i+1] = SuperArray[i];
++		i ++;
++	    }
++	    SuperArray = copy;
++	    numElements++;
++	}
++	    
++    }
++
++    public Object remove(int index){
++	if (index>= 0 && index < SuperArray.length){
++	    Object saved = SuperArray[index];
++	    Object[] copy = new Object[SuperArray.length];
++	    int i = 0;
++	    while (i<index){
++		copy[i] = SuperArray[i];
++		i ++;
++	    }
++	    while (i+1<SuperArray.length){
++		copy[i] = SuperArray[i];
++		i ++;
++	    }
++	    SuperArray = copy;
++	    numElements --;
++	    return saved;
++	}else{
++	    System.out.println("Index out of range");
++	    return null;
++	}
++    }
++
++    public int size(){
++	int counter = 0;
++	int i = 0;
++	while (i<SuperArray.length){
++	    if (SuperArray[i] != null) counter ++;
++	    i ++;
++	}
++	return counter;
++    }
++
++    public void resize(int newCapacity){
++	if (newCapacity > SuperArray.length){
++	Object[] copy = new Object[newCapacity];
++	int i = 0;
++	while (i<SuperArray.length){
++	    copy[i] = SuperArray[i];
++	    i ++;
++	}
++	SuperArray = copy;
++	}
++    }
++
++    public void clear(){
++	int i = 0;
++	while (i<SuperArray.length){
++	    SuperArray[i] = null;
++	    i ++;
++	}
++    }
++
++    public Object get(int index){
++	if (index>=0 && index< SuperArray.length){
++		return SuperArray[index];    
++	} else {
++	    System.out.println("Index is out of range");
++	    return null;
++	}
++    }
++
++    public Object set(int index, Object o){
++	if (index>= 0 && index<SuperArray.length){
++	    Object saved = SuperArray[index];
++	    SuperArray[index] = o;
++	    return saved;
++	} else {
++	    System.out.println("Index out of range");
++	    return null;
++	}
++    }
++
++}
