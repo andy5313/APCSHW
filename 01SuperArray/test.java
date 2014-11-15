497f26fda8de331271b631924f0e1fd47d36c33f
andy5313 <andyz5313@yahoo.com>
2014-11-15 15:15:21 -0500

    dfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdf
    dddddddddddddddddddd

---
+++ 01SuperArray/test.java
@@ -0,0 +1,13 @@
+public class test{
+    public static void main(String[] args){
+	Integer x = new Integer(5);
+	SuperArray L = new SuperArray();
+	L.add(x);
+	L.add(new Integer(99)); 
+	System.out.println(L.toString());
+	System.out.println(L.remove(1));
+	System.out.println(L.remove(10));
+	L.set(2,"A");
+	System.out.println(L.toString());
+    }
+}
