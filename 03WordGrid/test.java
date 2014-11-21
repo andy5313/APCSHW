c2559c5a62d5969a7607d4e0e4cd36952c5bf9cc
andy5313 <andyz5313@yahoo.com>
2014-11-20 22:18:41 -0500

    Update test.java

--- 03WordGrid/test.java
+++ 03WordGrid/test.java
@@ -1,3 +1,4 @@
+
 public class test{
     public static void main(String[] args){
 	WordGrid w = new WordGrid(10,10);
@@ -5,6 +6,8 @@ public class test{
 	w.addWordHorizontal("wooooo",0,0);
 	w.addWordHorizontal("wooooo",6,3);
 	w.addWordHorizontal("wooooo",5,5);
+	w.addWordVertical("boo",1,1);
+	w.addWordDiagonal("gooo",3,2);
 	System.out.println(w);
 	w.clear();
 	System.out.println(w);
